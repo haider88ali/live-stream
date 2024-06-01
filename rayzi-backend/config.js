e654f3fe77728f501b5b05964df78ad80ff8d02a
haider88ali <ali88haider8875@gmail.com>
2024-06-01 19:39:00 +0600

    initial commit

---
+++ rayzi-backend/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key for API
+  SECRET_KEY: "mTRmlJ7Vdu",
+
+  //gmail credentials for send email
+  EMAIL: "EMAIL",
+  PASSWORD: "PASSWORD",
+
+  //secret key for jwt
+  JWT_SECRET: "zYUyMgExtS",
+
+  //baseURL
+  baseURL: "http://sklivestream.com/",
+
+  //firebase server key for send notification
+  SERVER_KEY: "SERVER_KEY",
+};
